971ba50b1e7ee59819b1919c656633e3184c0c5d
Carl Andersson <git@lillecarl.com>
2025-11-10 17:21:27 +0100

    unhelmify chaoskube
    Good to start with the little things

--- kubenix/chaoskube.nix
+++ kubenix/chaoskube.nix
@@ -10,40 +10,87 @@ in
 {
   options.${moduleName} = {
     enable = lib.mkEnableOption moduleName;
-    helmValues = lib.mkOption {
-      type = lib.types.anything;
-      default = { };
+    baseName = lib.mkOption {
+      type = lib.types.nonEmptyStr;
+      default = moduleName;
+    };
+    namespace = lib.mkOption {
+      type = lib.types.nonEmptyStr;
+      default = moduleName;
+    };
+    version = lib.mkOption {
+      type = lib.types.nonEmptyStr;
+      default = "v0.37.0";
+    };
+    args = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {
+        no-dry-run = ""; # This makes things go poof
+        interval = "15m";
+        minimum-age = "6h";
+        timezone = "Europe/Stockholm";
+      };
     };
   };
   config =
     let
-      version = "0.37.0";
+      fullName = "${cfg.namespace}-${cfg.baseName}";
+      toChaosArgs =
+        args: lib.mapAttrsToList (n: v: if lib.stringLength v > 0 then "--${n}=${v}" else "--${n}") args;
     in
     lib.mkIf cfg.enable {
-      helm.releases.${moduleName} = {
-        namespace = "kube-system";
-        chart = "${
-          builtins.fetchTree {
-            type = "github";
-            owner = "linki";
-            repo = "chaoskube";
-            ref = "v${version}";
+      kubernetes.resources.none = {
+        Namespace.${cfg.namespace} = { };
+        ClusterRole.${fullName}.rules = [
+          {
+            apiGroups = [ "" ];
+            resources = [ "pods" ];
+            verbs = [
+              "list"
+              "delete"
+            ];
           }
-        }/chart/chaoskube";
-
-        values = {
-          image.tag = "v${version}";
-          chaoskube = {
-            args = {
-              no-dry-run = ""; # This makes things go poof
-              interval = "15m";
-              minimum-age = "6h";
-              timezone = "Europe/Stockholm";
-              # labels = "k8s-app!=cilium"; # Don't kill networking
+          {
+            apiGroups = [ "" ];
+            resources = [ "events" ];
+            verbs = [ "create" ];
+          }
+        ];
+        ClusterRoleBinding.${fullName} = {
+          roleRef = {
+            apiGroup = "rbac.authorization.k8s.io";
+            kind = "ClusterRole";
+            name = fullName;
+          };
+          subjects = {
+            _namedlist = true;
+            ${cfg.baseName} = {
+              kind = "ServiceAccount";
+              namespace = cfg.namespace;
             };
           };
-        }
-        // cfg.helmValues;
+        };
+      };
+      kubernetes.resources.${cfg.namespace} = {
+        ServiceAccount.${cfg.baseName} = { };
+        Deployment.${cfg.baseName}.spec = {
+          strategy.type = "Recreate";
+          replicas = 1;
+          selector.matchLabels.app = cfg.baseName;
+          template = {
+            metadata.labels.app = cfg.baseName;
+            spec = {
+              serviceAccountName = cfg.baseName;
+              containers = {
+                _namedlist = true;
+                ${cfg.baseName} = {
+                  image = "ghcr.io/linki/chaoskube:${cfg.version}";
+                  args = toChaosArgs cfg.args;
+                };
+              };
+            };
+          };
+        };
       };
     };
 }
