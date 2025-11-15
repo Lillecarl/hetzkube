08132238f713a2bb56c8f21088da1e7bba425145
Carl Andersson <git@lillecarl.com>
2025-11-15 16:55:40 +0100

    Set Cilium operator to two replicas

--- kubenix/modules/cilium.nix
+++ kubenix/modules/cilium.nix
@@ -105,7 +105,7 @@ in
             # Enable NAT64
             nat46x64Gateway.enabled = true;
             # Cheap is good
-            operator.replicas = 1;
+            operator.replicas = 2;
             # Tunnel mode requires the least from the underlying network, as long as
             # hosts can communicate we're golden
             routingMode = "tunnel";
