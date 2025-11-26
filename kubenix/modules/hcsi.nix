4fc03a983b53cc143de34db3fda0c2ec0c30724b
Carl Andersson <git@lillecarl.com>
2025-11-26 13:07:11 +0100

    always deploy hcsi to kube-system
    there's no use in overriding the namespace, just more work switching
    namespaces all the time

--- kubenix/modules/hcsi.nix
+++ kubenix/modules/hcsi.nix
@@ -10,10 +10,6 @@ in
 {
   options.${moduleName} = {
     enable = lib.mkEnableOption moduleName;
-    namespace = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = moduleName;
-    };
     apiToken = lib.mkOption {
       type = lib.types.str;
     };
@@ -25,12 +21,7 @@ in
   config = lib.mkIf cfg.enable {
     importyaml.${moduleName} = {
       src = "https://raw.githubusercontent.com/hetznercloud/csi-driver/v${cfg.version}/deploy/kubernetes/hcloud-csi.yml";
-      overrideNamespace = cfg.namespace;
     };
-    kubernetes.resources.none.Namespace.${cfg.namespace} = { };
-    kubernetes.resources.${cfg.namespace}.Secret.hcloud.stringData.token = cfg.apiToken;
-    # Override which namespace the role should bind to
-    kubernetes.resources.none.ClusterRoleBinding.hcloud-csi-controller.subjects.hcloud-csi-controller.namespace =
-      lib.mkForce cfg.namespace;
+    kubernetes.resources.kube-system.Secret.hcloud.stringData.token = cfg.apiToken;
   };
 }
