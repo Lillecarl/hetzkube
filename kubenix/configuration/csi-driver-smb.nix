0b10b5146eda2e9411de97ad3f79acc3f1351293
lillecarl <git@lillecarl.com>
2026-01-19 11:02:45 +0000

    disable something csi cifs samba doohickey

--- kubenix/configuration/csi-driver-smb.nix
+++ kubenix/configuration/csi-driver-smb.nix
@@ -9,7 +9,7 @@
 {
   config = lib.mkIf (config.stage == "full") {
     # \\u531666-sub1.your-storagebox.de\u531666-sub1
-    csi-driver-smb.enable = true;
-    kubernetes.resources.kube-system.ExternalSecret.sb1-kube = hlib.eso.mkBasic "name:hcloud-sb1-kube";
+    csi-driver-smb.enable = false;
+    # kubernetes.resources.kube-system.ExternalSecret.sb1-kube = hlib.eso.mkBasic "name:hcloud-sb1-kube";
   };
 }
