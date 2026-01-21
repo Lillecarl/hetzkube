8ca45d86bd7a32a3c7f3aa7158f2910324bca60b
lillecarl <git@lillecarl.com>
2026-01-21 15:47:43 +0000

    remove pgadmin launcher

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -17,23 +17,4 @@ in
   python-jsonpath = prev.python3Packages.callPackage ./python-jsonpath.nix { };
   kr8s = prev.python3Packages.callPackage ./kr8s.nix { inherit (final) python-jsonpath; };
   cheapam = prev.python3Packages.callPackage ../cheapam { };
-  pgadmin-launcher =
-    prev.writeScriptBin "pgadmin-launcher" # bash
-      ''
-        #! ${prev.runtimeShell}
-        set -euo pipefail
-        export PATH=${
-          lib.makeBinPath [
-            prev.coreutils-full
-          ]
-        }:$PATH
-        mkdir --parents /var/lib/pgadmin
-        mkdir --parents /var/log/pgadmin
-        (
-          echo $INITIAL_EMAIL
-          echo $INITIAL_PASSWORD
-          echo $INITIAL_PASSWORD
-        ) | ${lib.getExe' prev.pgadmin4 "pgadmin4-cli"} setup-db
-        exec ${lib.getExe prev.pgadmin4} "$@"
-      '';
 }
