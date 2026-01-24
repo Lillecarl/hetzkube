1e7a11aa4939287a98d8b8dd4e1d8526f0887329
lillecarl <git@lillecarl.com>
2026-01-24 00:06:12 +0000

    update hcsi

--- kubenix/modules/hcsi.nix
+++ kubenix/modules/hcsi.nix
@@ -12,7 +12,7 @@ in
     enable = lib.mkEnableOption moduleName;
     version = lib.mkOption {
       type = lib.types.nullOr lib.types.str;
-      default = "2.18.2";
+      default = "2.18.3";
     };
   };
   config = lib.mkIf cfg.enable {
