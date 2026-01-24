c31b6d4b70b4b14400612ea352e5bff3324382f2
lillecarl <git@lillecarl.com>
2026-01-24 00:06:12 +0000

    use fetchtree for external-secrets

--- kubenix/modules/external-secrets.nix
+++ kubenix/modules/external-secrets.nix
@@ -18,143 +18,130 @@ in
     };
     version = lib.mkOption {
       type = lib.types.nonEmptyStr;
-      default = "1.2.1";
-    };
-    sha256 = lib.mkOption {
-      type = lib.types.str;
-      default = "sha256-7NMQ/2qMoxUhBhkTt+qb7zQJmBwV6KE7QfU2ACm6tRU=";
+      default = "1.3.1";
     };
     helmValues = lib.mkOption {
       type = lib.types.anything;
       default = { };
     };
   };
-  config =
-    let
-      src = builtins.fetchTree {
-        type = "github";
-        owner = "external-secrets";
-        repo = "external-secrets";
-        ref = "helm-chart-${cfg.version}";
-      };
-    in
-    lib.mkIf cfg.enable {
-      kubernetes.resources.none.ClusterGenerator.crappy-pw = {
-        spec = {
-          kind = "Password";
-          generator.passwordSpec = {
-            length = 16;
-            symbols = 0;
-          };
+  config = lib.mkIf cfg.enable {
+    kubernetes.resources.none.ClusterGenerator.crappy-pw = {
+      spec = {
+        kind = "Password";
+        generator.passwordSpec = {
+          length = 16;
+          symbols = 0;
         };
-      };
-      helm.releases.${moduleName} = {
-        namespace = "kube-system";
-        chart = pkgs.fetchHelm {
-          chart = "external-secrets";
-          repo = "https://charts.external-secrets.io";
-          inherit (cfg) version sha256;
-        };
-
-        values = lib.recursiveUpdate {
-          image.tag = lib.mkDefault "v${cfg.version}";
-          certManager.enabled = lib.mkDefault config.cert-manager.enable;
-        } cfg.helmValues;
-      };
-      hlib.eso = rec {
-        mkBasic = swIdentifier: {
-          spec = {
-            refreshInterval = cfg.refreshInterval;
-            secretStoreRef = {
-              kind = "ClusterSecretStore";
-              name = "scaleway";
-            };
-            target.template.type = "kubernetes.io/basic-auth";
-            data = [
-              {
-                secretKey = "username";
-                remoteRef = {
-                  key = swIdentifier;
-                  property = "username";
-                };
-              }
-              {
-                secretKey = "password";
-                remoteRef = {
-                  key = swIdentifier;
-                  property = "password";
-                };
-              }
-            ];
-          };
-        };
-        mkToken = swIdentifier: mkOpaque swIdentifier "token";
-        mkOpaque = swIdentifier: secretKey: {
-          spec = {
-            refreshInterval = cfg.refreshInterval;
-            secretStoreRef = {
-              kind = "ClusterSecretStore";
-              name = "scaleway";
-            };
-            target.template.type = "Opaque";
-            data = [
-              {
-                inherit secretKey;
-                remoteRef.key = swIdentifier;
-              }
-            ];
-          };
-        };
-      };
-      kubernetes.apiMappings = {
-        ACRAccessToken = "generators.external-secrets.io/v1alpha1";
-        CloudsmithAccessToken = "generators.external-secrets.io/v1alpha1";
-        ClusterExternalSecret = "external-secrets.io/v1";
-        ClusterGenerator = "generators.external-secrets.io/v1alpha1";
-        ClusterPushSecret = "external-secrets.io/v1alpha1";
-        ClusterSecretStore = "external-secrets.io/v1";
-        ECRAuthorizationToken = "generators.external-secrets.io/v1alpha1";
-        ExternalSecret = "external-secrets.io/v1";
-        Fake = "generators.external-secrets.io/v1alpha1";
-        GCRAccessToken = "generators.external-secrets.io/v1alpha1";
-        GeneratorState = "generators.external-secrets.io/v1alpha1";
-        GithubAccessToken = "generators.external-secrets.io/v1alpha1";
-        Grafana = "generators.external-secrets.io/v1alpha1";
-        MFA = "generators.external-secrets.io/v1alpha1";
-        Password = "generators.external-secrets.io/v1alpha1";
-        PushSecret = "external-secrets.io/v1alpha1";
-        QuayAccessToken = "generators.external-secrets.io/v1alpha1";
-        SSHKey = "generators.external-secrets.io/v1alpha1";
-        STSSessionToken = "generators.external-secrets.io/v1alpha1";
-        SecretStore = "external-secrets.io/v1";
-        UUID = "generators.external-secrets.io/v1alpha1";
-        VaultDynamicSecret = "generators.external-secrets.io/v1alpha1";
-        Webhook = "generators.external-secrets.io/v1alpha1";
-      };
-      kubernetes.namespacedMappings = {
-        ACRAccessToken = true;
-        CloudsmithAccessToken = true;
-        ClusterExternalSecret = false;
-        ClusterGenerator = false;
-        ClusterPushSecret = false;
-        ClusterSecretStore = false;
-        ECRAuthorizationToken = true;
-        ExternalSecret = true;
-        Fake = true;
-        GCRAccessToken = true;
-        GeneratorState = true;
-        GithubAccessToken = true;
-        Grafana = true;
-        MFA = true;
-        Password = true;
-        PushSecret = true;
-        QuayAccessToken = true;
-        SSHKey = true;
-        STSSessionToken = true;
-        SecretStore = true;
-        UUID = true;
-        VaultDynamicSecret = true;
-        Webhook = true;
       };
     };
+    helm.releases.${moduleName} = {
+      namespace = "kube-system";
+
+      chart = builtins.fetchTree {
+        type = "tarball";
+        url = "https://github.com/external-secrets/external-secrets/releases/download/helm-chart-${cfg.version}/external-secrets-${cfg.version}.tgz";
+      };
+
+      values = lib.recursiveUpdate {
+        image.tag = lib.mkDefault "v${cfg.version}";
+        certManager.enabled = lib.mkDefault config.cert-manager.enable;
+      } cfg.helmValues;
+    };
+    hlib.eso = rec {
+      mkBasic = swIdentifier: {
+        spec = {
+          refreshInterval = cfg.refreshInterval;
+          secretStoreRef = {
+            kind = "ClusterSecretStore";
+            name = "scaleway";
+          };
+          target.template.type = "kubernetes.io/basic-auth";
+          data = [
+            {
+              secretKey = "username";
+              remoteRef = {
+                key = swIdentifier;
+                property = "username";
+              };
+            }
+            {
+              secretKey = "password";
+              remoteRef = {
+                key = swIdentifier;
+                property = "password";
+              };
+            }
+          ];
+        };
+      };
+      mkToken = swIdentifier: mkOpaque swIdentifier "token";
+      mkOpaque = swIdentifier: secretKey: {
+        spec = {
+          refreshInterval = cfg.refreshInterval;
+          secretStoreRef = {
+            kind = "ClusterSecretStore";
+            name = "scaleway";
+          };
+          target.template.type = "Opaque";
+          data = [
+            {
+              inherit secretKey;
+              remoteRef.key = swIdentifier;
+            }
+          ];
+        };
+      };
+    };
+    kubernetes.apiMappings = {
+      ACRAccessToken = "generators.external-secrets.io/v1alpha1";
+      CloudsmithAccessToken = "generators.external-secrets.io/v1alpha1";
+      ClusterExternalSecret = "external-secrets.io/v1";
+      ClusterGenerator = "generators.external-secrets.io/v1alpha1";
+      ClusterPushSecret = "external-secrets.io/v1alpha1";
+      ClusterSecretStore = "external-secrets.io/v1";
+      ECRAuthorizationToken = "generators.external-secrets.io/v1alpha1";
+      ExternalSecret = "external-secrets.io/v1";
+      Fake = "generators.external-secrets.io/v1alpha1";
+      GCRAccessToken = "generators.external-secrets.io/v1alpha1";
+      GeneratorState = "generators.external-secrets.io/v1alpha1";
+      GithubAccessToken = "generators.external-secrets.io/v1alpha1";
+      Grafana = "generators.external-secrets.io/v1alpha1";
+      MFA = "generators.external-secrets.io/v1alpha1";
+      Password = "generators.external-secrets.io/v1alpha1";
+      PushSecret = "external-secrets.io/v1alpha1";
+      QuayAccessToken = "generators.external-secrets.io/v1alpha1";
+      SSHKey = "generators.external-secrets.io/v1alpha1";
+      STSSessionToken = "generators.external-secrets.io/v1alpha1";
+      SecretStore = "external-secrets.io/v1";
+      UUID = "generators.external-secrets.io/v1alpha1";
+      VaultDynamicSecret = "generators.external-secrets.io/v1alpha1";
+      Webhook = "generators.external-secrets.io/v1alpha1";
+    };
+    kubernetes.namespacedMappings = {
+      ACRAccessToken = true;
+      CloudsmithAccessToken = true;
+      ClusterExternalSecret = false;
+      ClusterGenerator = false;
+      ClusterPushSecret = false;
+      ClusterSecretStore = false;
+      ECRAuthorizationToken = true;
+      ExternalSecret = true;
+      Fake = true;
+      GCRAccessToken = true;
+      GeneratorState = true;
+      GithubAccessToken = true;
+      Grafana = true;
+      MFA = true;
+      Password = true;
+      PushSecret = true;
+      QuayAccessToken = true;
+      SSHKey = true;
+      STSSessionToken = true;
+      SecretStore = true;
+      UUID = true;
+      VaultDynamicSecret = true;
+      Webhook = true;
+    };
+  };
 }
