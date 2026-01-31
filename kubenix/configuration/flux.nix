f4dd2178416cad119d24747dc066b210cd1e0599
lillecarl <git@lillecarl.com>
2026-01-31 13:38:44 +0000

    deploy CRD with flux by default

--- kubenix/configuration/flux.nix
+++ kubenix/configuration/flux.nix
@@ -5,5 +5,26 @@
       enable = true;
       version = "2.7.5";
     };
+
+    kubernetes.transformers = [
+      (
+        resource:
+        if resource.kind == "HelmRelease" then
+          lib.recursiveUpdate {
+            spec = {
+              install = {
+                crds = "CreateReplace";
+                remediation.retries = 3;
+              };
+              upgrade = {
+                crds = "CreateReplace";
+                remediation.retries = 3;
+              };
+            };
+          } resource
+        else
+          resource
+      )
+    ];
   };
 }
