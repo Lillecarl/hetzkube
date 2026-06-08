65a747e2e302e9082ef96876f6151dac169d7b66
lillecarl <git@lillecarl.com>
2026-06-08 23:54:49 +0200

    Updated kube-state-metrics from 2.18.0 to 2.19.0

--- kubenix/configuration/victoriametrics.nix
+++ kubenix/configuration/victoriametrics.nix
@@ -26,7 +26,7 @@
       };
       kube-state-metrics = {
         enable = true;
-        version = "2.18.0";
+        version = "2.19.0";
         inherit namespace;
       };
       grafana-operator = {
