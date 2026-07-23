36cbf186f07d36a217ed990ebcefbeb63fd1410b
lillecarl <git@lillecarl.com>
2026-07-23 13:35:02 +0200

    external-secrets: bump 2.6.0 -> 2.8.0

--- kubenix/modules/external-secrets.nix
+++ kubenix/modules/external-secrets.nix
@@ -18,7 +18,7 @@ in
     };
     version = lib.mkOption {
       type = lib.types.nonEmptyStr;
-      default = "2.6.0";
+      default = "2.8.0";
     };
     helmValues = lib.mkOption {
       type = lib.types.anything;
