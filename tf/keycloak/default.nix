e7db534276f931d041703a03fd127ec2791c8cb8
lillecarl <git@lillecarl.com>
2026-07-23 11:10:13 +0200

    keycloak: make the auth realm's clients presentable (Account Console applications list)
    Add description, base_url/root_url/web_origins, always_display_in_console,
    and a logoUri (via extra_config, since terraform-provider-keycloak doesn't
    model it directly) to all 5 OIDC clients. Enable consent_required +
    display_on_consent_screen so Keycloak's Account Console actually renders
    logoUri -- it's only populated from a real UserConsentModel
    (AccountRestService#modelToRepresentation), not unconditionally.

--- tf/keycloak/default.nix
+++ tf/keycloak/default.nix
@@ -146,6 +146,7 @@ in
     resource.keycloak_openid_client.kubernetes = mkKC {
       client_id = "kubernetes";
       name = "Kubernetes";
+      description = "kubectl/kubelogin OIDC authentication against the hetzkube cluster API server.";
 
       valid_redirect_uris = [
         # Kubelogin runs some local webserver here when setting up OIDC tokens
@@ -156,6 +157,21 @@ in
       direct_access_grants_enabled = false;
       service_accounts_enabled = false;
       access_type = "PUBLIC";
+
+      # Presentation: show up in the Account Console's Applications list
+      # (and stay listed even before a user's first kubectl login), with a
+      # consent screen + icon like any other real-world SSO client instead
+      # of silently minting tokens. Account Console only ever renders
+      # `logoUri` (client.attributes.logoUri, see Keycloak's
+      # AccountRestService#modelToRepresentation) once a UserConsentModel
+      # exists for that user+client, hence consent_required here too.
+      always_display_in_console = true;
+      consent_required = true;
+      display_on_consent_screen = true;
+      consent_screen_text = "access the Kubernetes cluster API on your behalf";
+      extra_config = {
+        logoUri = "https://raw.githubusercontent.com/cncf/artwork/main/projects/kubernetes/icon/color/kubernetes-icon-color.png";
+      };
     };
 
     resource.keycloak_openid_user_realm_role_protocol_mapper.kubernetes = mkKC {
@@ -166,20 +182,34 @@ in
       multivalued = true;
     };
 
-    resource.keycloak_openid_client.headlamp = mkKC {
-      client_id = "headlamp";
-      name = "Headlamp";
+    resource.keycloak_openid_client.headlamp =
+      let
+        host = "headlamp.lillecarl.com";
+      in
+      mkKC {
+        client_id = "headlamp";
+        name = "Headlamp";
+        description = "Web UI for browsing and managing the hetzkube cluster.";
 
-      valid_redirect_uris = [
-        "https://headlamp.lillecarl.com/oidc-callback"
-      ];
+        valid_redirect_uris = [ "https://${host}/oidc-callback" ];
+        base_url = "https://${host}";
+        root_url = "https://${host}";
+        web_origins = [ "https://${host}" ];
 
-      standard_flow_enabled = true;
-      direct_access_grants_enabled = false;
-      service_accounts_enabled = false;
-      access_type = "PUBLIC";
-      access_token_lifespan = "28800"; # 8 hour tokens
-    };
+        standard_flow_enabled = true;
+        direct_access_grants_enabled = false;
+        service_accounts_enabled = false;
+        access_type = "PUBLIC";
+        access_token_lifespan = "28800"; # 8 hour tokens
+
+        always_display_in_console = true;
+        consent_required = true;
+        display_on_consent_screen = true;
+        consent_screen_text = "view and manage Kubernetes resources on your behalf";
+        extra_config = {
+          logoUri = "https://raw.githubusercontent.com/cncf/artwork/main/projects/headlamp/icon/color/headlamp-icon-color.png";
+        };
+      };
 
     resource.keycloak_openid_user_realm_role_protocol_mapper.headlamp = mkKC {
       client_id = config.resource.keycloak_openid_client.headlamp "id";
@@ -197,20 +227,34 @@ in
       add_to_id_token = true;
     };
 
-    resource.keycloak_openid_client.grafana = mkKC {
-      client_id = "grafana";
-      name = "Grafana";
+    resource.keycloak_openid_client.grafana =
+      let
+        host = "grafana.lillecarl.com";
+      in
+      mkKC {
+        client_id = "grafana";
+        name = "Grafana";
+        description = "Dashboards and metrics/logs exploration for the hetzkube cluster.";
 
-      valid_redirect_uris = [
-        "https://grafana.lillecarl.com/login/generic_oauth"
-      ];
+        valid_redirect_uris = [ "https://${host}/login/generic_oauth" ];
+        base_url = "https://${host}";
+        root_url = "https://${host}";
+        web_origins = [ "https://${host}" ];
 
-      standard_flow_enabled = true;
-      direct_access_grants_enabled = true;
-      service_accounts_enabled = false;
-      access_type = "PUBLIC";
-      access_token_lifespan = "28800"; # 8 hour tokens
-    };
+        standard_flow_enabled = true;
+        direct_access_grants_enabled = true;
+        service_accounts_enabled = false;
+        access_type = "PUBLIC";
+        access_token_lifespan = "28800"; # 8 hour tokens
+
+        always_display_in_console = true;
+        consent_required = true;
+        display_on_consent_screen = true;
+        consent_screen_text = "view your profile and role for dashboard access";
+        extra_config = {
+          logoUri = "https://raw.githubusercontent.com/grafana/grafana/main/public/img/grafana_icon.svg";
+        };
+      };
 
     resource.keycloak_openid_group_membership_protocol_mapper.grafana = mkKC {
       client_id = resource.keycloak_openid_client.grafana "id";
@@ -220,20 +264,34 @@ in
       full_path = false;
     };
 
-    resource.keycloak_openid_client.argocd = mkKC {
-      client_id = "argocd";
-      name = "ArgoCD";
+    resource.keycloak_openid_client.argocd =
+      let
+        host = "argocd.lillecarl.com";
+      in
+      mkKC {
+        client_id = "argocd";
+        name = "ArgoCD";
+        description = "GitOps continuous delivery for the hetzkube cluster.";
 
-      valid_redirect_uris = [
-        "https://argocd.lillecarl.com/auth/callback"
-      ];
+        valid_redirect_uris = [ "https://${host}/auth/callback" ];
+        base_url = "https://${host}";
+        root_url = "https://${host}";
+        web_origins = [ "https://${host}" ];
 
-      standard_flow_enabled = true;
-      direct_access_grants_enabled = false;
-      service_accounts_enabled = false;
-      access_type = "PUBLIC";
-      access_token_lifespan = "28800"; # 8 hour tokens
-    };
+        standard_flow_enabled = true;
+        direct_access_grants_enabled = false;
+        service_accounts_enabled = false;
+        access_type = "PUBLIC";
+        access_token_lifespan = "28800"; # 8 hour tokens
+
+        always_display_in_console = true;
+        consent_required = true;
+        display_on_consent_screen = true;
+        consent_screen_text = "view your profile and role for GitOps sync/deploy access";
+        extra_config = {
+          logoUri = "https://raw.githubusercontent.com/cncf/artwork/main/projects/argo/icon/color/argo-icon-color.png";
+        };
+      };
 
     resource.keycloak_openid_user_realm_role_protocol_mapper.argocd = mkKC {
       client_id = config.resource.keycloak_openid_client.argocd "id";
@@ -250,6 +308,7 @@ in
       mkKC {
         client_id = "pgadmin";
         name = "pgAdmin4";
+        description = "Web-based administration for the cluster's PostgreSQL databases.";
 
         valid_redirect_uris = [
           "https://${host}/oauth2/authorize"
@@ -263,6 +322,14 @@ in
         direct_access_grants_enabled = false;
         service_accounts_enabled = false;
         access_type = "PUBLIC";
+
+        always_display_in_console = true;
+        consent_required = true;
+        display_on_consent_screen = true;
+        consent_screen_text = "view your profile for database administration access";
+        extra_config = {
+          logoUri = "https://raw.githubusercontent.com/pgadmin-org/pgadmin4/master/web/pgadmin/static/img/logo-256.png";
+        };
       };
   };
 }
