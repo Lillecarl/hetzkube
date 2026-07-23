597454929977d9fa658f43921df4d7a6d4607fa9
lillecarl <git@lillecarl.com>
2026-07-23 13:35:02 +0200

    cert-manager: bump 1.20.2 -> 1.21.0

--- kubenix/configuration/cert-manager.nix
+++ kubenix/configuration/cert-manager.nix
@@ -12,7 +12,7 @@ in
   config = lib.mkIf (config.stage == "full") {
     cert-manager = {
       enable = true;
-      version = "1.20.2";
+      version = "1.21.0";
     };
     kubernetes.resources.cert-manager = {
       ExternalSecret.cloudflare = hlib.eso.mkToken "name:cloudflare-token";
